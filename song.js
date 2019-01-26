f7abe90e1ab6e4d729b1621959ad7c95f9d8be33
kushser <kush_ser@ukr.net>
2019-01-26 17:38:03 +0200

    Initial commit

---
+++ song.js
@@ -0,0 +1,35 @@
+var song = "Natural";
+var band = "Imagine Dragons";
+var release ="July 17, 2018";
+var album = "Origin";
+// song duration
+var durationInSeconds = 189 ;
+var songWriters = ["Mattias Larson", "Dan Raynolds ", "Ben McKee", "Justin Drew Tranter","Daniel James Platzman","Wayne Sermon"];
+var imagineDragons = {
+    lead: "Dan Raynolds",
+    acousticGuitar: "Wayne Sermon",
+    bassGuitar: "Ben McKee",
+    drums: "Daniel James Platzman"
+};
+var viewsOnYoutube = 165369958;
+// number of likes
+var likes = 2300000;
+var songAbout ="finding yourself and being willing and able to stand up to whatever adversity comes your way";
+/*
+data output to
+the consol
+*/
+console.log("My favorite son is " + song);
+console.log(band);
+console.log("Realese: " + release);
+console.log( "Album: " + album);
+console.log("Duration: " + durationInSeconds + " sec");
+console.log( "Writers: " + songWriters);
+console.log("Imagine Dragons:");
+console.log("Lead: " + imagineDragons.lead);
+console.log("acousticGuitar: " + imagineDragons.acousticGuitar);
+console.log("bassGuitar: " + imagineDragons. bassGuitar);
+console.log("drums: " + imagineDragons.drums);
+console.log("Youtube: " + viewsOnYoutube + " views" );
+console.log("Likes: " + likes);
+console.log("About: " + songAbout);
